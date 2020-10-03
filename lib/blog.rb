a6c8d9e3fc6cb3777cf2844c924580e31b5f1754
Seiei Miyagi <hanachin@gmail.com>
2020-10-04 08:54:45 +0900

    Remove /+

--- lib/blog.rb
+++ lib/blog.rb
@@ -3,8 +3,8 @@
 module Blog
   class << self
     def call(environment)
-      # TODO: limit max nest level, and compact PATH_INFO, ex. gsub(%r{/+}, '/')
-      ::Blog::Resource.find(environment.fetch('PATH_INFO'), ::Root).call(environment)
+      # TODO: limit max nest level
+      ::Blog::Resource.find(environment.fetch('PATH_INFO').gsub(%r{/+}, '/'), ::Root).call(environment)
     end
   end
 end
