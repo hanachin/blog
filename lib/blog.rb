8def69647075f1cf579e9e39dc284a866adc9d5f
Seiei Miyagi <hanachin@gmail.com>
2020-10-04 08:52:19 +0900

    Add Resource.find

--- lib/blog.rb
+++ lib/blog.rb
@@ -4,7 +4,7 @@ module Blog
   class << self
     def call(environment)
       # TODO: limit max nest level, and compact PATH_INFO, ex. gsub(%r{/+}, '/')
-      ::Blog::Resource.new(environment.fetch('PATH_INFO'), current_resource: ::Root).call(environment)
+      ::Blog::Resource.find(environment.fetch('PATH_INFO'), ::Root).call(environment)
     end
   end
 end
--- lib/blog/resource.rb
+++ lib/blog/resource.rb
@@ -2,24 +2,24 @@
 
 module Blog
   class Resource
-    def initialize(path, current_resource:)
-      @resource = find_resource(path, current_resource)
+    class << self
+      def find(path, current_resource)
+        return new(current_resource) if path.empty?
+
+        resource, sub_resource = path.split('/', 2)
+        found_resource = resource.empty? ? current_resource : current_resource.find(resource)
+        find(sub_resource || '', found_resource)
+      rescue
+        # TODO: 404
+      end
+    end
+
+    def initialize(resource)
+      @resource = resource
     end
 
     def call(environment)
       @resource.public_send(environment.fetch('REQUEST_METHOD'), environment)
-    end
-
-    private
-
-    def find_resource(path, current_resource)
-      return current_resource if path.empty?
-
-      resource, sub_resource = path.split('/', 2)
-      found_resource = resource.empty? ? current_resource : current_resource.find(resource)
-      find_resource(sub_resource || '', found_resource)
-    rescue
-      # TODO: 404
     end
   end
 end
